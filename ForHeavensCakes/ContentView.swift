8b9cd8efd5e44af2678cc717fe7b184bc6b9a243
AtinAgnihotri <atinagnihotri@gmail.com>
2021-06-20 20:18:34 +0530

    Codable conformance for ObservableObjects

--- ForHeavensCakes/ContentView.swift
+++ ForHeavensCakes/ContentView.swift
@@ -7,6 +7,25 @@
 
 import SwiftUI
 
+class User: ObservableObject, Codable {
+    
+    enum CodingKeys: CodingKey {
+        case name
+    }
+    
+    @Published var name = "User TestUser"
+    
+    required init(from decoder: Decoder) throws {
+        let containter = try decoder.container(keyedBy: CodingKeys.self)
+        name = try containter.decode(String.self, forKey: .name)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self.name, forKey: .name)
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
