53fbcc303ef10797ac32389936f6dd5f4e78e395
Atin Agnihotri <atin.agnihotri@paytmmoney.com>
2021-06-20 19:50:03 +0530

    Initial Commit

---
+++ ForHeavensCakes/ForHeavensCakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForHeavensCakesApp.swift
+//  ForHeavensCakes
+//
+//  Created by Atin Agnihotri on 20/06/21.
+//
+
+import SwiftUI
+
+@main
+struct ForHeavensCakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
