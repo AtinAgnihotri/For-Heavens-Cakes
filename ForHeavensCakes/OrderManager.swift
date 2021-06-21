501d7fd7c11434ccd9ff766b5ceb045a65200c90
AtinAgnihotri <atinagnihotri@gmail.com>
2021-06-21 20:29:45 +0530

    Adds Cost property to OrderManager

--- ForHeavensCakes/OrderManager.swift
+++ ForHeavensCakes/OrderManager.swift
@@ -38,4 +38,15 @@ class OrderManager: ObservableObject {
     static func getInstance() -> OrderManager {
         self.instance
     }
+    
+    var cost: Double{
+        var costPerCupcake = 2.0 // Basic cost of cupcake
+        costPerCupcake += extraFrosting ? 1 : 0 // Charge for extra frosting per cupcake
+        costPerCupcake += addSprinkles ? 0.5 : 0 // Charge for adding sprinkles per cupcake
+        
+        // Extra charge for type of cupcake in ascending order
+        let chargePerType = Double(type) / 2 // Vanilla: 0, Strawberry: 1, Chocolate: 1.5, Rainbow: 2
+        
+        return (chargePerType + (costPerCupcake * Double(quantity)))
+    }
 }
